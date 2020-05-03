729187738d6da9d35109f470087c06a1d9037b33
Victor <57220247+victorwlin@users.noreply.github.com>
2020-05-03 20:04:09 +0800

    Adding homepage to package.json

---
+++ docs/precache-manifest.fba60d0e3458ec09aaeff39f4af18202.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5959d8710a024a14dc1eeae2fe41979e",
+    "url": "/fitness2/index.html"
+  },
+  {
+    "revision": "6eb1d770343d258b12e9",
+    "url": "/fitness2/static/js/2.317b9a36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fitness2/static/js/2.317b9a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae8df7729d7fdb3c164",
+    "url": "/fitness2/static/js/main.1597f21d.chunk.js"
+  },
+  {
+    "revision": "49258b9ea44428482c45",
+    "url": "/fitness2/static/js/runtime-main.ffdaf607.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2be13c6a4ddeab0edcb2c4a437216e35.js"
+  "/fitness2/precache-manifest.fba60d0e3458ec09aaeff39f4af18202.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fitness2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
